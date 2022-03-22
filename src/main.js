acc0a4bd0ed9c1b253310e5711053c3a9f999020
INMD1 <lyw514549@gmail.com>
2022-03-23 02:47:27 +0900

    router 버전 완료

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import Home from './views/HomeView.vue'
+import about from './views/AboutView.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: about
+    }
+  ]
+})
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
